11ab6894e640f7b2fa16195af0c875f44c8582f7
Kassandra Flores <kassyflores2013@gmail.com>
2024-09-25 12:07:19 -0500

    Made all pictures the same size

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { faker } from '@faker-js/faker'
+
 const fullname = faker.person.fullName()
 const firstname = faker.person.firstName()
 const lastname = faker.person.lastName()
@@ -13,7 +14,7 @@ const username = faker.internet.userName({firstName: firstname, lastName:lastnam
 <template>
     <!-- {{ fullname }} <br /> -->
     <div class ="border border-gray-400 rounded-xl shadow bg-gray-200 h-full">
-        <img :src="faker.image.urlLoremFlickr({category: 'cats'})"  class="rounded-t-xl" />
+        <img :src="faker.image.urlLoremFlickr({category: 'cats'})"  class="rounded-t-xl object-cover w-full h-64" />
         <div class="p-4">
             <p class="mb-2 text-2x1 font-bold">{{ fullname }}</p>
             <p class="mb-3 font-normal text-gray-700">{{ job_title }}</p>
